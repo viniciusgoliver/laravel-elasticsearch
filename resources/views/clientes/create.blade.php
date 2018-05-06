9fd3feb1235c99bb5e282150a3aad2d2b2b08f13
Vinícius G. Oliveira <vinicius.oliver@gmail.com>
2018-05-05 22:06:50 -0300

    Primeiro Commit

---
+++ resources/views/clientes/create.blade.php
@@ -0,0 +1,27 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <h3>Novo cliente</h3>
+
+        {!! Form::open(['route'=>'clientes.store', 'class'=>'form']) !!}
+        {{csrf_field()}}
+        <div class="form-group">
+            {!! Form::label('name', 'Nome:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('cpf', 'CPF:') !!}
+            {!! Form::text('cpf', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Criar', ['class'=>'btn btn-primary']) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+    </div>
+
+@endsection
